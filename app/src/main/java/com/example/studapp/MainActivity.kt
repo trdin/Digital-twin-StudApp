410dff617648f400af72b2472327b52abd2961ed
Peter <trdin.peter@gmail.com>
2022-12-25 18:06:20 +0100

    measuring decibels, interval SMLTG-135

--- app/src/main/java/com/example/studapp/MainActivity.kt
+++ app/src/main/java/com/example/studapp/MainActivity.kt
@@ -6,9 +6,7 @@ import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
+import android.os.*
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
@@ -16,18 +14,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.IOException
+import kotlin.math.log10
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
-    private var output: String? = null
-    private var mediaRecorder: MediaRecorder? = null
-    private var state: Boolean = false
-    private var recordingStopped: Boolean = false
+
     lateinit var button_start_recording: Button;
     lateinit var button_stop_recording:Button;
     lateinit var button_pause_recording:Button;
 
-    lateinit var soundMeter: SoundMeter;
+    lateinit var mainHandler: Handler
+
+    private var updateTextTask: Runnable?  = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,107 +39,61 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, permissions,0)
         }
 
-        mediaRecorder = MediaRecorder()
-        output = Environment.getExternalStorageDirectory().absolutePath + "/recording1.mp3"
-        Log.d("aaa", Environment.getExternalStorageDirectory().absolutePath );
-
-        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
-        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-        mediaRecorder?.setOutputFile(output)
-
         button_start_recording = findViewById(R.id.button_start_recording)
-        /*button_start_recording.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                ActivityCompat.requestPermissions(this, permissions,0)
-            }
-            startRecording()
-        }*/
 
-        soundMeter = SoundMeter();
-        soundMeter.context = this;
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED ) {
+           updateTextTask =   object : Runnable {
+                override fun run() {
+                    var noiseRecorder = NoiseRecorder();
+                    noiseRecorder.context = this@MainActivity;
+                    //Log.d("aaa", noiseRecorder.noiseLevel.toString())
+                    mainHandler.postDelayed(this, 10000)
+                    button_start_recording.text = round(noiseRecorder.noiseLevel).toString();
+                }
+            }
+        }
+
+
+
 
         button_start_recording.setOnClickListener {
-            soundMeter.start()
+            Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
         }
 
 
         button_stop_recording = findViewById(R.id.button_stop_recording)
 
         button_stop_recording.setOnClickListener{
-            //stopRecording()
-            soundMeter.stop()
-            Toast.makeText(this, "Decibels: ${soundMeter.amplitude}", Toast.LENGTH_SHORT).show()
+
+            Toast.makeText(this, "Decibels: soundMeter.amp", Toast.LENGTH_SHORT).show()
 
         }
 
         button_pause_recording = findViewById(R.id.button_pause_recording)
 
         button_pause_recording.setOnClickListener {
-            pauseRecording()
+
+
+        }
+
+        mainHandler = Handler(Looper.getMainLooper())
+    }
+
+
+
+
+    override fun onPause() {
+        super.onPause()
+        if(updateTextTask != null){
+            mainHandler.removeCallbacks(updateTextTask!!)
         }
     }
 
-    private fun startRecording() {
-        try {
-            mediaRecorder?.prepare()
-            mediaRecorder?.start()
-            state = true
-            Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
-        } catch (e: IllegalStateException) {
-            e.printStackTrace()
-            Toast.makeText(this,  e.toString(), Toast.LENGTH_SHORT).show()
-        } catch (e: IOException) {
-            e.printStackTrace()
-            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    @SuppressLint("RestrictedApi", "SetTextI18n")
-    @TargetApi(Build.VERSION_CODES.N)
-    private fun pauseRecording() {
-        if(state) {
-            if(!recordingStopped){
-                Toast.makeText(this,"Stopped!", Toast.LENGTH_SHORT).show()
-                mediaRecorder?.pause()
-                recordingStopped = true
-                button_pause_recording.text = "Resume"
-            }else{
-                resumeRecording()
-            }
-        }
-    }
-
-    @SuppressLint("RestrictedApi", "SetTextI18n")
-    @TargetApi(Build.VERSION_CODES.N)
-    private fun resumeRecording() {
-        Toast.makeText(this,"Resume!", Toast.LENGTH_SHORT).show()
-        mediaRecorder?.resume()
-        button_pause_recording.text = "Pause"
-        recordingStopped = false
-    }
-
-    private fun stopRecording(){
-        if(state){
-
-            mediaRecorder?.stop()
-            var amplitude = mediaRecorder?.maxAmplitude
-            var  decibels = 0.0
-            if (amplitude != null) {
-                Log.d("aaa", "amplitude: $amplitude")
-                decibels = 20 * Math.log10(amplitude / 2700.0)
-            }
-
-            mediaRecorder?.release()
-
-
-            state = false
-            Toast.makeText(this, "Decibels: ${decibels}", Toast.LENGTH_SHORT).show()
-        }else{
-            Toast.makeText(this, "You are not recording right now!", Toast.LENGTH_SHORT).show()
+    override fun onResume() {
+        super.onResume()
+        if(updateTextTask != null) {
+            mainHandler.post(updateTextTask!!)
         }
     }
 }
---
+++ app/src/main/java/com/example/studapp/NoiseRecorder.kt
@@ -0,0 +1,93 @@
+package com.example.studapp
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import android.util.Log
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+class NoiseRecorder {
+    private val TAG: String = "aaa"
+    lateinit var context: Context;
+    //the value 51805.5336 can be derived from asuming that x=32767=0.6325 Pa and x=1 = 0.00002 Pa (the reference value)
+
+    //making the buffer bigger....
+    //recording data;
+    //x=max;
+    val noiseLevel: Double
+        // calculating the pascal pressure based on the idea that the max amplitude (between 0 and 32767) is
+        // relative to the pressure
+        get() {
+            Log.e(TAG, "start new recording process")
+            var bufferSize = AudioRecord.getMinBufferSize(
+                44100,
+                AudioFormat.CHANNEL_IN_DEFAULT,
+                AudioFormat.ENCODING_PCM_16BIT
+            )
+            //making the buffer bigger....
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                bufferSize = bufferSize * 4
+                val recorder = AudioRecord(
+                    MediaRecorder.AudioSource.MIC,
+                    44100,
+                    AudioFormat.CHANNEL_IN_DEFAULT,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSize
+                )
+
+                val data = ShortArray(bufferSize)
+                var average = 0.0
+
+                /*runBlocking { // this: CoroutineScope
+                    launch { // launch a new coroutine and continue*/
+                        recorder.startRecording()
+                        //delay(2000L) // non-blocking delay for 1 second (default time unit is ms)
+                        //println("World!") // print after delay
+                        recorder.read(data, 0, bufferSize)
+                        recorder.stop()
+                        Log.e(TAG, "stop")
+                        for (s in data) {
+                            if (s > 0) {
+                                average += Math.abs(s.toInt()).toDouble()
+                            } else {
+                                bufferSize--
+                            }
+                        }
+                        //x=max;
+                        val x = average / bufferSize
+                        Log.e(TAG, "" + x)
+                        recorder.release()
+                        Log.d(TAG, "getNoiseLevel() ")
+                        var db = 0.0
+
+                        // calculating the pascal pressure based on the idea that the max amplitude (between 0 and 32767) is
+                        // relative to the pressure
+                        val pressure =
+                            x / 51805.5336 //the value 51805.5336 can be derived from asuming that x=32767=0.6325 Pa and x=1 = 0.00002 Pa (the reference value)
+                        Log.d(TAG, "x=$pressure Pa")
+                        db = 20 * Math.log10(pressure / 0.00002)
+                        Log.d(TAG, "db=$db")
+
+                   /* }
+                }*/
+                return db;
+                //recording data;
+
+            }
+            return 0.0
+
+        }
+
+}
