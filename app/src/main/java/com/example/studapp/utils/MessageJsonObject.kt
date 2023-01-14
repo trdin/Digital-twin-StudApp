513e6a5e8cf175a30493b77056b378a0bc854899
Oskar Dolenc <oskar.dolenc@student.um.si>
2023-01-14 16:03:39 +0100

    SMLTG-141 objavljanje sporocil

---
+++ app/src/main/java/com/example/studapp/utils/MessageJsonObject.kt
@@ -0,0 +1,11 @@
+package com.example.studapp.utils
+
+class MessageJsonObject {
+    var latitude:String = ""
+    var longitude: String = ""
+    var time: String=""
+
+    var content: String = ""
+    var category: String = ""
+
+}
